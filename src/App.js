dcb477e5b3ad416ba46028fc61126a4984658b65
nemoprime86 <stevenmater@gmail.com>
2022-02-07 12:39:07 +0100

    did css

--- src/App.js
+++ src/App.js
@@ -21,45 +21,54 @@ function App() {
     if (resultA > resultB) {
       setResults('Player A wins this round!');
       setScoreA(scoreA + 1);
-      setResultA(0);
-      setResultB(0);
+      resetResults();
     } else if (resultA < resultB) {
       setResults('Player B wins this round!');
       setScoreB(scoreB + 1);
-      setResultA(0);
-      setResultB(0);
+      resetResults();
     } else {
       setResults("It's a tie!");
-      setResultA(0);
-      setResultB(0);
+      resetResults();
     }
   };
 
   // Reset
-  const reset = () => {
+  const resetGame = () => {
     setScoreA(0);
     setScoreB(0);
-    // setResults('Game has been reset!');
+    setResults('Game has been reset!');
+  };
+
+  const resetResults = () => {
+    setResultA(0);
+    setResultB(0);
   };
 
   // Winner
   useEffect(() => {
     if (scoreA === 11) {
-      setResults('Player A wins the game!!! Congratulations');
-      reset();
+      alert('Player A wins the game!!! Congratulations');
+      resetGame();
     } else if (scoreB === 11) {
-      setResults('Player B wins the game!!! Congratulations');
-      reset();
+      alert('Player B wins the game!!! Congratulations');
+      resetGame();
     }
   }, [scoreA, scoreB]);
 
   return (
     <div className="app">
-      <h1>Score</h1>
-      <div>Player A: {scoreA}</div>
-      <div>Player B: {scoreB}</div>
-      <br />
+      <h1>Dice Game</h1>
+      <button onClick={resetGame}>New Game</button>
       <div className="card">
+        <h3>Scoreboard</h3>
+        <p>
+          <b>Player A:</b> {scoreA}
+        </p>
+        <p>
+          <b>Player B:</b> {scoreB}
+        </p>
+      </div>
+      <div className="cardRollA">
         Player A<br />
         {resultA}
         <br />
@@ -75,8 +84,11 @@ function App() {
           <button>Rolled</button>
         )}
       </div>
-      <br />
       <div className="card">
+        <p>Results: {results}</p>
+        <button onClick={compareResults}>Compare Rolls</button>
+      </div>
+      <div className="cardRollB">
         Player B<br />
         {resultB}
         <br />
@@ -92,11 +104,6 @@ function App() {
           <button>Rolled</button>
         )}
       </div>
-      <p>Results: {results}</p>
-      <button onClick={compareResults}>Compare Rolls</button>
-      <br />
-      <br />
-      <button onClick={reset}>New Game</button>
     </div>
   );
 }
